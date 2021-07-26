171f9ae80a3c75b81cbc2fa9baeb8a5889fef616
Artem Tebenkov <magnydd@gmail.com>
2021-07-26 17:58:27 +0400

    fix: better format

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-$(function(){
-    var mixer = mixitup('.gallery__content');
+$(function () {
+  var mixer = mixitup(".gallery__content");
 
-    $('.blog__slider').slick({
-        dots: true,
-        arrows: false,
-    });
+  $(".blog__slider").slick({
+    dots: true,
+    arrows: false,
+  });
 });
