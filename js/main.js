2f50144b2d79df9170c6e26bbb7f9c121f6dc946
Artem Tebenkov <magnydd@gmail.com>
2021-07-29 16:23:13 +0400

    add full adaptive

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 $(function () {
-  var mixer = mixitup(".gallery__content");
 
   $(".blog__slider").slick({
     dots: true,
     arrows: false,
   });
+
+  $('.menu__toggle').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+
+  });
+
+  var mixer = mixitup(".gallery__content");
 });
