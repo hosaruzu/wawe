d57c9034db3746d436e291d2433cbf6d93f43ab0
Artem Tebenkov <magnydd@gmail.com>
2021-08-02 19:15:11 +0400

    add smooth scroll to ancors and sticky header menu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
 $(function () {
 
+    $(window).scroll(function() {
+      if ($('.header__top').offset().top > 40) {
+        $('.header__top').addClass('header__top--active')
+      } else {
+        $('.header__top').removeClass('header__top--active')
+      }
+    });
+
+  $(".menu a, .header__top-inner a").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({
+      scrollTop: top
+    }, 1000);
+  });
+
   $(".blog__slider").slick({
     dots: true,
     arrows: false,
   });
 
-  $('.menu__toggle').on('click', function () {
+  $('.menu__toggle, .menu__list').on('click', function () {
     $('.menu__list').toggleClass('menu__list--active');
 
   });
